68fcbcc2050a88b6728025e5b048446268767c44
Caio Kurek <caiokurek@hotmail.com>
2023-04-03 17:20:19 -0300

    Create Aula07_Kurek.py

---
+++ P_O_O/aula 07 - Caixa eletrônico/Aula07_Kurek.py
@@ -0,0 +1,31 @@
+class Cliente():
+    def __init__(self, nome, CPF):
+        self.nome = nome
+        self.__CPF = CPF
+        self.__saldo = 0
+        self.__limite = 2000
+        
+    def exibeCPF(self):
+       print("CPF: %s" %(self.__CPF))
+        
+    def exibeSaldo(self):
+       print("Saldo atual: R$ %.2f" %(self.__saldo))
+        
+    def deposito(self, i) :
+        self.__saldo += i
+        
+    def pagamento(self, i):
+        if (self.__saldo + self.__limite > i):
+            self.__saldo -= i
+        else:
+            print("ERRO! Saldo + Limite insuficiente")
+        
+eu = Cliente("Caio","123.456.789-00")
+
+eu.exibeCPF()
+
+eu.deposito(1000)
+eu.exibeSaldo()
+
+eu.pagamento(2500)
+eu.exibeSaldo()
