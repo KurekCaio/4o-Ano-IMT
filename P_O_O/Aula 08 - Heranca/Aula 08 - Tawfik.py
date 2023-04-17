e92261b926820184f6d535ae9062518e1332ebb1
Tawfik Metwally <60214510+Tawfik-Metwally@users.noreply.github.com>
2023-04-17 18:11:05 -0300

    Falta limitar os ranges

--- P_O_O/Aula 08 - Heranca/Aula 08 - Tawfik.py
+++ P_O_O/Aula 08 - Heranca/Aula 08 - Tawfik.py
@@ -1,1 +1,54 @@
-print("oi")
+class Dispositivo():
+    def __init__(self, nome):
+        self.nome = nome
+
+    def retornaID(self):
+        return 'Nome:' + self.nome
+
+class Atuador(Dispositivo):
+    def __init__(self, nome, estado):
+        super().__init__(nome)
+        self.estado = estado
+        
+    def exibeEstado(self):
+            print(super().retornaID())
+            if(self.estado == 1):
+                return 'Estado: ligado'
+            elif(self.estado == 0):
+                return 'Estado: desligado'
+    
+class LED(Atuador):
+    def __init__(self, nome, estado):
+        super().__init__(nome, estado)
+        
+    def liga(self):
+        self.estado = 1
+        
+    def desliga(self):
+        self.estado = 0
+    
+class Motor(Atuador):
+    def __init__(self, nome, estado, direcao, velocidade):
+        super().__init__(nome, estado)
+        self.direcao = direcao
+        self.velocidade = velocidade
+        
+    def atribuiDirecao(self, direcao):
+        self.direcao = direcao
+            
+    def atribuiVelocidade(self, velocidade):
+        self.velocidade = velocidade
+    
+    def exibeEstado(self):
+        print(super().exibeEstado())
+        if(self.estado == 1):
+            if(self.direcao == 1):
+                return ('direcao: Horário\nvelocidade: %i' %(self.velocidade))
+            elif(self.direcao == 0):
+                return ('direcao: Anti-Horário\nvelocidade: %i' %(self.velocidade))
+        else:
+            return ''
+
+x=Motor("nome", 1, 0, 200)
+
+print(x.exibeEstado())
